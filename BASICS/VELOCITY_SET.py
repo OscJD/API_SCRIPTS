4e56ab16f976888dce93d25140c733fff8853408
OscJD <oscar5-juarez@hotmail.com>
2017-05-30 18:29:18 -0500

    Update VELOCITY_SET.py

--- BASICS/VELOCITY_SET.py
+++ BASICS/VELOCITY_SET.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 # create flyt_python navigation class instance
-from flyt_python import API
-drone = API.navigation()
+from flyt_python import api
+import time
+drone = api.navigation()
 # wait for interface to initialize
 time.sleep(3.0)
 
